13624c1a6fa5b9d3b4031709902187eb5d8fab69
JestAK <peckpech07@gmai.com>
2024-03-04 15:26:36 +0100

    Added parameters for text, Made parameter able to be changed with Inspector, Added text tracks on timeline

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,3 +1,5 @@
+import anime from "animejs";
+
 import TextInspector from "./TextInspector";
 import './App.css';
 import React, {useEffect, useState} from "react";
@@ -6,14 +8,17 @@ import Draggable from "react-draggable";
 function App() {
 
     class TextObject {
-        static idCounter = 0;
         constructor() {
-            this.id = TextObject.idCounter++;
+            this.id = textIdCounter;
             this.posX = 0;
             this.posY = 0;
             this.content = `Text ID - ${textIdCounter}`;
             this.keyframes = [];
+            this.textColor = "#ffffff";
+            this.fontSize = 20;
             this.strokeThickness = 3;
+            this.strokeColor = "#000000"
+            setTextIdCounter(textIdCounter + 1)
         }
     }
 
@@ -22,7 +27,7 @@ function App() {
     const [timelineRange, setTimelineRange] = useState(null);
     const [textElements, setTextElements] = useState([]);
     const [textIdCounter, setTextIdCounter] = useState(0);
-    const [isHidden, setIsHidden] = useState(false);
+    const [isHidden, setIsHidden] = useState(true);
     const [selectedTextObject, setSelectedTextObject] = useState({})
 
     //get video and timeline-range
@@ -36,10 +41,8 @@ function App() {
 
     const addText = (newTextElement) => {
         const newTextElements = [...textElements];
-        newTextElement.id = textIdCounter;
         newTextElements.push(newTextElement);
         setTextElements(newTextElements);
-        setTextIdCounter(textIdCounter + 1)
     };
 
     const timelineToCurrentTime = () => {
@@ -48,6 +51,37 @@ function App() {
 
     const currentTimeToTimeline = () => {
         timelineRange.value = (video.currentTime / video.duration) * 100;
+    }
+
+    const onHandleIsHiddenChange = (newValue) => {
+        setIsHidden(newValue)
+    }
+
+    const onHandleTextElementsChange = (newValue) => {
+        setTextElements(newValue)
+        console.log(textElements)
+    }
+
+    const getSelectedTextObject = (id) => {
+        const foundObject = textElements.find(obj => obj.id === id);
+
+        if (!foundObject) {
+            alert(`Object not found with id: ${id}`);
+        }
+
+        setSelectedTextObject(foundObject)
+    }
+
+    const handleTextPosChange = (event, data, id) => {
+        const index = textElements.findIndex((element) => element.id === id);
+        const newTextElements = [...textElements];
+        console.log(data)
+        newTextElements[index].posX = data.x
+        newTextElements[index].posY = data.y
+        console.log(newTextElements[index].posX)
+        console.log(data.x)
+
+        setTextElements(newTextElements);
     }
 
   return (
@@ -62,16 +96,36 @@ function App() {
               </video>
 
               {textElements.map((textElement) => (
-                  <Draggable bounds="parent">
-                      <p key={textElement.id}
-                         style={{position: 'absolute', top: textElement.top, left: textElement.left}}
-                         className="subtitle-text">
+                  <Draggable
+                      bounds="parent"
+                      key={textElement.id}
+                      position={{ x: textElement.posX, y: textElement.posY }}
+                      onDrag={(event, data) => {
+                      handleTextPosChange(event, data, textElement.id)
+                      getSelectedTextObject(textElement.id)
+                  }}>
+                      <pre style={{position: 'absolute',
+                             color: textElement.textColor,
+                             fontSize: `${textElement.fontSize}px`,
+                             WebkitTextStrokeWidth: `${textElement.strokeThickness}px`,
+                             WebkitTextStrokeColor: textElement.strokeColor,
+                      }}
+                         className="subtitle-text"
+                         onDoubleClick={() => {
+                             getSelectedTextObject(textElement.id)
+                             setIsHidden(false)
+                         }}>
                           {textElement.content}
-                      </p>
+                      </pre>
                   </Draggable>
               ))}
 
-              <TextInspector />
+              <TextInspector TextObject={selectedTextObject}
+                             isHidden={isHidden}
+                             onHandleIsHiddenChange={onHandleIsHiddenChange}
+                             textElements={textElements}
+                             onHandleTextElementsChange={onHandleTextElementsChange}
+              />
           </div>
           <div id="controllers">
               <button id="stop-button" onClick={() => {
@@ -99,6 +153,17 @@ function App() {
               defaultValue={0}
               onInput={() => {timelineToCurrentTime()}}
           />
+
+            {textElements.map((textElement) => (
+                <div className="text-track" key={textElement.id}>
+                    <div className="text-track-id" onClick={() => {
+                        getSelectedTextObject(textElement.id)
+                        setIsHidden(false)
+                    }}>ID: {textElement.id}</div>
+                    <div className="text-track-keyframe-area"></div>
+                </div>
+            ))}
+
         </div>
       </>
   );
@@ -106,3 +171,14 @@ function App() {
 
 
 export default App;
+
+
+//Add when can upload video
+// {video ? (
+//     <video id="video" onTimeUpdate={() => { currentTimeToTimeline() }}>
+//         <source src={videoSrc} type="video/mp4" />
+//         Your browser does not support the video tag.
+//     </video>
+// ) : (
+//     <div id="viewport-empty-video">NO VIDEO</div>
+// )}
--- my-app/src/TextInspector.jsx
+++ my-app/src/TextInspector.jsx
@@ -1,27 +1,171 @@
 import './App.css';
 
-function TextInspector({TextObject, isHidden}) {
+function TextInspector({TextObject, isHidden, onHandleIsHiddenChange, textElements, onHandleTextElementsChange}) {
+
+    const isHiddenChange = () => {
+        const newValue = true
+        onHandleIsHiddenChange(newValue)
+    }
+
+    const deleteTextElement = (id) => {
+        const newTextElements = textElements.filter(element => element.id !== id)
+        onHandleTextElementsChange(newTextElements)
+        console.log(textElements)
+    }
+
+    const changeTextElementContent = (e, id) => {
+        const index = textElements.findIndex((element) => element.id === id);
+        const newTextElements = [...textElements];
+        newTextElements[index].content = e.target.value;
+        onHandleTextElementsChange(newTextElements)
+    }
+
+    const changeTextElementColor = (e, id) => {
+        const index = textElements.findIndex((element) => element.id === id);
+        const newTextElements = [...textElements];
+        newTextElements[index].textColor = e.target.value;
+        onHandleTextElementsChange(newTextElements)
+    }
+
+    const changeTextElementFontSize = (e, id) => {
+        const index = textElements.findIndex((element) => element.id === id);
+        const newTextElements = [...textElements];
+        newTextElements[index].fontSize = e.target.value;
+        onHandleTextElementsChange(newTextElements)
+    }
+
+    const changeTextElementStrokeThickness = (e, id) => {
+        const index = textElements.findIndex((element) => element.id === id);
+        const newTextElements = [...textElements];
+        newTextElements[index].strokeThickness = e.target.value;
+        onHandleTextElementsChange(newTextElements)
+    }
+
+    const changeTextElementStrokeColor = (e, id) => {
+        const index = textElements.findIndex((element) => element.id === id);
+        const newTextElements = [...textElements];
+        newTextElements[index].strokeColor = e.target.value;
+        onHandleTextElementsChange(newTextElements)
+    }
+
+    const changeTextElementPosition = (e, id) => {
+        const index = textElements.findIndex((element) => element.id === id);
+        const newTextElements = [...textElements];
+        const { name, value } = e.target;
+        const newPosition = parseFloat(value) || 0;
+
+        if (name === "PosX") {
+            newTextElements[index].posX = newPosition;
+        } else if (name === "PosY") {
+            newTextElements[index].posY = newPosition;
+        }
+
+        onHandleTextElementsChange(newTextElements);
+    };
+
 
     return(
       <>
-          <div id="text-inspector" style={{ visibility: isHidden ? 'hidden' : 'visible' }}>
-              <p>Text ID</p>
+          <div id="text-inspector" style={{visibility: isHidden ? 'hidden' : 'visible'}}>
+              <p>Text ID: {TextObject.id}</p>
 
               <label htmlFor="PosX">Position X:</label>
-              <input type="number" id="PosX" name="PosX" required minLength="4" maxLength="8" size="10"/>
+              <input
+                  type="number"
+                  id="PosX"
+                  name="PosX"
+                  required
+                  minLength="1"
+                  maxLength="4"
+                  value={TextObject.posX}
+                  onChange={(e) => {changeTextElementPosition(e, TextObject.id)}}
+              />
 
               <label htmlFor="PosY">Position Y:</label>
-              <input type="number" id="PosY" name="PosY" required minLength="4" maxLength="8" size="10"/>
+              <input
+                  type="number"
+                  id="PosY"
+                  name="PosY"
+                  required
+                  minLength="1"
+                  maxLength="4"
+                  value={TextObject.posY}
+                  onChange={(e) => {changeTextElementPosition(e, TextObject.id)}}
+              />
 
               <label htmlFor="content">Content:</label>
-              <input type="text" id="content" name="content" required minLength="4" maxLength="8" size="10"/>
+              <textarea
+                  id="content"
+                  name="content"
+                  required minLength="1"
+                  value={TextObject.content}
+                  onChange={(e) => changeTextElementContent(e, TextObject.id)}
+              />
+
+              <label htmlFor="text-color">Text color:</label>
+              <input
+                  type="color"
+                  id="text-color"
+                  name="text-color"
+                  required minLength="7"
+                  maxLength="7"
+                  value={TextObject.textColor}
+                  onChange={(e) => changeTextElementColor(e, TextObject.id)}
+              />
+
+              <label htmlFor="font-size">Font size:</label>
+              <input
+                  type="number"
+                  id="font-size"
+                  name="font-size"
+                  required
+                  minLength="1"
+                  maxLength="3"
+                  min="1"
+                  value={TextObject.fontSize}
+                  onChange={(e) => changeTextElementFontSize(e, TextObject.id)}
+              />
 
               <label htmlFor="stroke-thickness">Stroke thickness:</label>
-              <input type="number" id="stroke-thickness" name="stroke-thickness" required minLength="4" maxLength="8"
-                     size="10"/>
+              <input
+                  type="number"
+                  id="stroke-thickness"
+                  name="stroke-thickness"
+                  required
+                  minLength="1"
+                  maxLength="2"
+                  min="0"
+                  value={TextObject.strokeThickness}
+                  onChange={(e) => changeTextElementStrokeThickness(e, TextObject.id)}
+              />
 
-              <button id="delete-button">Delete</button>
-              <button id="close-button" onClick={() => {console.log("Hello")}}>Close</button>
+              <label htmlFor="stroke-color">Stroke color:</label>
+              <input
+                  type="color"
+                  id="stroke-color"
+                  name="stroke-color"
+                  required
+                  minLength="7"
+                  maxLength="7"
+                  value={TextObject.strokeColor}
+                  onChange={(e) => changeTextElementStrokeColor(e, TextObject.id)}
+              />
+
+              <button id="close-button" onClick={() => {
+                  console.log("Keyframe added")
+              }}>Add keyframe
+              </button>
+
+              <button id="delete-button" onClick={() => {
+                  deleteTextElement(TextObject.id)
+                  isHiddenChange()
+              }}>Delete
+              </button>
+
+              <button id="close-button" onClick={() => {
+                  isHiddenChange()
+              }}>Close
+              </button>
           </div>
       </>
     );
